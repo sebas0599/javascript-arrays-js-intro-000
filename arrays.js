d1ec3ab58e21ae2c6d9c68a415839e3097236fde
sebastian <sverdu6@gmail.com>
2020-03-09 22:00:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = array;
+  newArray.unshift(item);
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(item)
+  return array
+}
