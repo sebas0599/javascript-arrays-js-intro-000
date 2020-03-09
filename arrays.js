afcb3137f3aef887d9ca5c86433757e8cd089ff1
sebastian <sverdu6@gmail.com>
2020-03-09 22:06:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  var newArray = array;
+  newArray.push(element)
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
